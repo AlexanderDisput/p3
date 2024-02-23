720432e820d65251ef2ce3601a6e8ac7e1b9b935
Alexander Disput <alexanderdisput@yahoo.com>
2024-02-23 12:08:33 +0000

    Added function to ask user for desired password length and validation of ipnut

--- run.py
+++ run.py
@@ -47,3 +47,39 @@
 
 #     Add a cryptographic layer to encrypt the data sent to doc 
 #     add functionality to pull and decrypt information from doc 
+
+
+def password_length():
+    """
+    Asks user for input of integer between 8 and 40.
+    Try will validate if the input is an int and if the int is between 8 and 40
+    """
+    print("How long do you want your password to be? \n")
+
+    while True:
+        user_input = input("Please enter an NUMBER between 8 and 40\n")
+        if validate_input(user_input):
+            print("Input valid\n")
+            break
+    
+    return user_input
+
+
+def validate_input(user_password_length):
+    """
+    Checks the user input and confirms that it is an integer between 8 and 40
+    """
+    try:
+        user_password_length = int(user_password_length)
+    except ValueError:
+        print(f"Invalid input! Must be an integer!\n")
+        return False
+
+    if not 8 <= user_password_length <= 40:
+        print("Invalid input! Integer must be between 8 and 40\n")
+        return False
+
+    return True
+
+password_length()
+
